59772c56dacb16ced5a3df697a37ec3b634ca427
HyunHee111624 <144989249+HyunHee111624@users.noreply.github.com>
2023-11-04 00:47:39 +0900

    02_02

--- main.c
+++ main.c
@@ -6,15 +6,22 @@
 int main(int argc, char *argv[])
 {
   int grade [N_STUDENT];
-  int i;
+  int i, average;
+  int sum;
   
   printf("input 5 scores : ");
   
+  sum = 0;
+  
   for(i=0; i<N_STUDENT; i++)
+  {
     scanf("%d", &grade[i]);
-    
-  for(i=0; i<N_STUDENT; i++)
-    printf("grade[%i} = %d\n", i, grade[i]);
+    sum += grade[i];
+  } 
+  
+  average = sum / N_STUDENT;
+  
+  printf("score average : %i\n", average);
   
   system("PAUSE");	
   return 0;
